f7eab4912469eb03bd0ef44f756f5367d360c953
NovaFox161 <s.phoenix161@gmail.com>
2016-11-26 15:50:59 -0600

    Initial commit

---
+++ src/main/java/com/cloudcraftgaming/dayz/Main.java
@@ -0,0 +1,18 @@
+package com.cloudcraftgaming.dayz;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Nova Fox on 11/26/2016.
+ * Website: www.cloudcraftgaming.com
+ * For Project: DayZ
+ */
+public class Main extends JavaPlugin {
+    public static Main plugin;
+
+    public void onDisable() {}
+
+    public void onEnable() {
+        plugin = this;
+    }
+}
